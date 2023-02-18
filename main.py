d47926ca50e2d2f1fb07b673257904a55265a780
jacobhanson <jhanreg11@gmail.com>
2023-02-17 18:14:08 -0800

    google initial

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import requests, os
+import os
+
+import requests
+
+
 # Main Method, runs code
 def main():
     auth_token = os.environ["auth_token"]
@@ -48,10 +52,45 @@ def yes_counter(topic, url, headers):
     result_counter["no"] = no_counter;
     return result_counter;
     
-# Runs main method.
-main()
+# # Runs main method.
+# main()
 
+import os.path
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
+def build_service():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    
+    return build("sheets", "v4", credentials=creds)
 
+spreadsheet_id = "1wEGu3snEQlqEahaJ_FMMyYilWYAfykBbvZEwVaQvjiE"
+range_name = "A:I"
+service = build_service()
+res = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
